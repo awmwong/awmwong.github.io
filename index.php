372aafeeaa5f080c398f696171251ffb5a559677
Anthony Wong <awmwong@gmail.com>
2012-01-26 00:42:03 -0500

    New site!

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Anthony Wong</title>
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+    <link href='http://fonts.googleapis.com/css?family=Vollkorn' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Qwigley' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+  </head>
+
+  <body>
+
+    <div id="wrapper">
+
+      <div id="card">
+
+        <h1>Anthony Wong</h1>
+        <h2>uWaterloo Software Engineering Undergraduate</h2>
+
+        <ul>
+          <li><a href="mailto:contact@awong.ca" class="email" title="Email">Email</a></li>
+          <li><a href="http://facebook.com/awmwong" class="facebook" title="Facebook">Facebook</a></li>
+          <li><a href="http://twitter.com/anthowong" class="twitter" title="Twitter">Twitter</a></li>
+          <li><a href="http://plus.google.com/109350295993730275387/" class="gplus" title="Google+">Google+</a></li>
+          <li><a href="http://ca.linkedin.com/pub/anthony-wong/24/487/aab" class="linkedin" title="LinkedIn">LinkedIn</a></li>
+          <li><a href="http://stackoverflow.com/users/931425/anthony-wong" class="stackoverflow" title="Stack Overlow">Stack Overflow</a></li>
+          <li><a href="http://github.com/awmwong" class="github" title="GitHub">GitHub</a></li>
+        </ul>
+
+      </div>
+
+    </div>
+
+    <footer>
+    &copy; <?php echo date('Y'); ?>, Anthony Wong. Design by <a href="http://timothy-armstrong.com">Timothy Armstrong</a>
+    </footer>
+
+  </body>
+
+</html>
