ce8341e11a35b2ea0c18c264f252a525bb841b45
Anthony Wong <awmwong@gmail.com>
2012-11-19 19:15:57 -0500

    updated twitter handle

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
         <ul>
           <li><a href="mailto:contact@awong.ca" class="email" title="Email">Email</a></li>
           <li><a href="http://facebook.com/awmwong" class="facebook" title="Facebook">Facebook</a></li>
-          <li><a href="http://twitter.com/anthowong" class="twitter" title="Twitter">Twitter</a></li>
+          <li><a href="http://twitter.com/wong" class="twitter" title="Twitter">Twitter</a></li>
           <li><a href="http://plus.google.com/109350295993730275387/" class="gplus" title="Google+">Google+</a></li>
           <li><a href="http://ca.linkedin.com/pub/anthony-wong/24/487/aab" class="linkedin" title="LinkedIn">LinkedIn</a></li>
           <li><a href="http://stackoverflow.com/users/931425/anthony-wong" class="stackoverflow" title="Stack Overlow">Stack Overflow</a></li>
